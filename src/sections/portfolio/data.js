92131362949aff1da23d6d38032f8bb9d56fc788
Muhammad Sadam Muneer <sadam.fullstackdev@gmail.com>
2025-03-06 22:12:11 +0500

    updated code with portfolio section

--- src/sections/portfolio/data.js
+++ src/sections/portfolio/data.js
@@ -3,23 +3,24 @@ import Image2 from "../../assets/project2.jpg";
 import Image3 from "../../assets/project3.jpg";
 import Image4 from "../../assets/project4.jpg";
 import Image5 from "../../assets/project5.jpg";
-import Image6 from "../../assets/project6.jpg";
+import Image8 from "../../assets/project8.jpg";
+
 
 const data = [
   {
     id: 1,
-    category: "UI/UX",
+    category: "Front-End",
     image: Image1,
-    title: "Treaty Health (UI/UX)",
-    desc: "A user-friendly and visually appealing healthcare platform designed to enhance accessibility and improve user experience. It offers seamless navigation and an intuitive interface for better healthcare management.",
+    title: "Treaty Health",
+    desc: "A user-friendly and visually appealing healthcare platform designed to enhance accessibility and improve user experience. It offers seamless navigation",
     demo: "https://treaty-health-frontend.vercel.app/",
-    github: "https://github.com/sadam-codes/",
+    github: "https://github.com/sadam-codes/treaty-health/",
   },
   {
     id: 2,
     category: "Front-End",
     image: Image2,
-    title: "My Landing Pages (Frontend)",
+    title: "Landing Pages",
     desc: "A modern and responsive landing page collection designed for various businesses and personal projects. Built with a focus on performance, aesthetics, and user experience.",
     demo: "https://mylandingpages.vercel.app",
     github: "https://github.com/sadam-codes/Landing-pages",
@@ -28,38 +29,39 @@ const data = [
     id: 3,
     category: "Front-End",
     image: Image3,
-    title: "Project Title Three (Frontend)",
-    desc: "Provident similique accusantium nemo autem. Veritatis obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam nihil, eveniet aliquid culpa officia aut!",
-    demo: "http://egatortutorials.com",
-    github: "https://github.com/sadam-codes/",
+    title: "Bikin Webpage",
+    desc: "A sleek and responsive webpage designed for businesses and personal branding. Built with modern frontend technologies to ensure a visually appealing and user-friendly experience.",
+    demo: "https://bikin-webpage.vercel.app/",
+    github: "https://github.com/sadam-codes/Bikin-webpage",
   },
   {
     id: 4,
-    category: "UI/UX",
+    category: "Front-End",
     image: Image4,
-    title: "Project Title Four (UIUX)",
+    title: "Project Title Four",
     desc: "Provident similique accusantium nemo autem. Veritatis obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam nihil, eveniet aliquid culpa officia aut!",
     demo: "http://egatortutorials.com",
     github: "https://github.com/sadam-codes/",
   },
   {
     id: 5,
-    category: "UI/UX",
+    category: "Front-End",
     image: Image5,
-    title: "Project Title Five (UIUX)",
+    title: "Project Title Five",
     desc: "Provident similique accusantium nemo autem. Veritatis obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam nihil, eveniet aliquid culpa officia aut! ",
     demo: "http://egatortutorials.com",
     github: "https://github.com/sadam-codes/",
   },
   {
     id: 6,
-    category: "Front-End",
-    image: Image6,
-    title: "Project Title Six (Frontend)",
+    category: "Back-End",
+    image: Image8,
+    title: "Project Title Eight (Backend)",
     desc: "Provident similique accusantium nemo autem. Veritatis obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam nihil, eveniet aliquid culpa officia aut!",
     demo: "http://egatortutorials.com",
     github: "https://github.com/sadam-codes/",
   },
+
 ];
 
 export default data;
